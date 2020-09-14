eadfb65680d04df1a4ef5d33a80f22d1a6609c4b
Ashish Gupta <ashishgupta1350@outlook.com>
2020-09-14 18:52:22 +0530

    add sql custom fetch code

--- Hibernate_XML_Mapping_OneToOne/src/main/java/com/ashish/hibernate/Test.java
+++ Hibernate_XML_Mapping_OneToOne/src/main/java/com/ashish/hibernate/Test.java
@@ -5,6 +5,10 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.query.Query;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
     public static void main(String[] args)
@@ -16,22 +20,63 @@ public class Test {
         Transaction transaction = session.beginTransaction();
 
         Employee employee = new Employee();
-        employee.setName("Ashish");
-        employee.setSalary(2000f);
+        employee.setName("afsdafadsfasd");
+        employee.setSalary(1346546f);
 
         Address address = new Address();
-        address.setCity("Telangana");
-        address.setCountry("india");
-        address.setState("Hyderabad");
+        address.setCity("dasfasd");
+        address.setCountry("afd");
+        address.setState("adsf");
+
+        // test create query method
+
 
 
         employee.setAddress(address);
         address.setEmployee(employee);
 
         session.save(employee);
+
+
+        System.out.println("\n\nCreate Query Method Test Start \n\n");
+
+        Query query = session.createSQLQuery("select * from emptable").addEntity(Employee.class);
+        List<Employee> result = query.list();
+        for(Employee x: result)
+        {
+            System.out.println(x.getEmpId());
+            System.out.println(x.getName());
+            System.out.println(x.getSalary());
+        }
+
+
+
+
+
+        System.out.println(result.toString());
+//        ArrayList<Employee> x = new ArrayList<>(result);
+//        for(Employee emp : result)
+//        {
+//            System.out.println(emp.getAddress());
+//            System.out.println(emp.getEmpId());
+//            System.out.println(emp.getName());
+//        }
+
+        System.out.println("\n\nCreate Query Method Test End \n\n");
+
+        System.out.println("------------\n");
+        Query q = session.createSQLQuery("select empid from emptable");
+        System.out.println(q.list().toString());
+        List y = q.list();
+        for(Object x: y)
+        {
+            System.out.println(Long.valueOf(x.toString()));
+        }
+        System.out.println("------------\n");
+
         transaction.commit();
         session.close();
 
-        System.out.println("Added employee to Database 1, table Address33_Table, Employee33_Table");
+        System.out.println("Added employee to Database 1, table addrtable, emptable");
     }
 }
